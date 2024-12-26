b61b256457fae6597e6e5f056ac5748310928d0e
Sergey Trushin <sergey.trushinn@proton.me>
2024-12-26 18:55:46 +0100

    feat: add solutions for countCats, sortByHeight, getSumOfDigits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  const sumOfDigits = n
+    .toString()
+    .split("")
+    .reduce((sum, digit) => sum + +digit, 0);
+
+  if (sumOfDigits > 9) return getSumOfDigits(sumOfDigits);
+
+  return sumOfDigits;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
